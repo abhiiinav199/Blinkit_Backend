4391e22a7cf081dccec145c34af66dd1acbfc59a
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-05-20 17:00:12 +0530

    wip - made api for searching the product in productController and added it into productRoute

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -288,6 +288,50 @@ export const deleteProductController = async (req, res) => {
   }
 }
 
+export const searchProduct = async (req, res) => {
+     try {
+        let { search, page , limit } = request.body 
+
+        if(!page){
+            page = 1
+        }
+        if(!limit){
+            limit  = 10
+        }
+
+        const query = search ? {
+            $text : {
+                $search : search
+            }
+        } : {}
+
+        const skip = ( page - 1) * limit
+
+        const [data,dataCount] = await Promise.all([
+            ProductModel.find(query).sort({ createdAt  : -1 }).skip(skip).limit(limit).populate('category subCategory'),
+            ProductModel.countDocuments(query)
+        ])
+
+        return response.json({
+            message : "Product data",
+            error : false,
+            success : true,
+            data : data,
+            totalCount :dataCount,
+            totalPage : Math.ceil(dataCount/limit),
+            page : page,
+            limit : limit 
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message || error,
+            error: true
+        })
+    }
+}
+
 // export const deleteImageController = async (req, res) => {
 //     try {
 //         const { publicId } = req.body
