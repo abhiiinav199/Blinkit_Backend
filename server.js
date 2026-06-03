014fb271a3dd185daa1a93b40c39d7b64a7f5ebb
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-06-03 21:05:48 +0530

    wip- added api for adding address in- server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import uploadRouter from './routes/upload.route.js'
 import subCategoryRouter from './routes/subCategory.route.js'
 import productRouter from './routes/product.route.js'
 import cartRouter from './routes/cart.route.js'
+import addressRouter from './routes/address.route.js'
 
 
 const app = express()
@@ -56,6 +57,7 @@ app.use("/api/file", uploadRouter)
 app.use("/api/subcategory", subCategoryRouter)
 app.use("/api/product", productRouter)
 app.use("/api/cart", cartRouter)
+app.use("/api/address", addressRouter)
 // const expireOtpTime = new Date().getTime() +60 *60 *1000
 // console.log(expireOtpTime)
 const startServer= async () =>{
