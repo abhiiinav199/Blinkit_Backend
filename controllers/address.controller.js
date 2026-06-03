e53e6cf237cbe44f43be057b8e5a7f7a96d3f724
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-06-03 20:55:37 +0530

    wip- made one api for adding address- address.controller.js

---
+++ controllers/address.controller.js
@@ -0,0 +1,38 @@
+import AddressModel from "../Models/address.model.js";
+
+export const addAddressController = async(req,res) =>{
+    try {
+        const userId = req.userId //middleware
+        const {address_line,city,state,country,pincode,mobile} = req?.body
+        if(!address_line || !city || !state || !country || !pincode || !mobile){
+            return res.status(400).json({
+                message : "All fields are required",
+                error : true,
+                success : false
+            })
+        }
+
+        const createAddress = await AddressModel.create({
+            address_line,
+            city,
+            state,
+            country,
+            pincode,
+            mobile
+        })
+
+        const addAddressUserId = await UserModel.findByIdAndUpdate(userId, { $push: { address_details : createAddress._id } })
+
+        return res.status(201).json({
+            message : "Address added successfully",
+            error : false,
+            success : true,
+            data : createAddress
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success : false,
+            message : error.message ||error
+        })
+    }
+}
