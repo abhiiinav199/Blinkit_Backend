5deb00521d3925b7ed3f1ff0594711616103d653
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-06-10 12:45:26 +0530

    wip-fixed typo in address.route.js

--- routes/address.route.js
+++ routes/address.route.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { auth } from "../middleware/auth.js";
-import { addAddressController, getAddressController, updateAddressController } from "../controllers/address.controller.js";
+import { addAddressController, getAddressController, updateAddressController, deleteAddressController } from "../controllers/address.controller.js";
 
 const addressRouter = Router()
 
 addressRouter.post("/create", auth, addAddressController)
 addressRouter.get("/get", auth, getAddressController)
 addressRouter.put("/update", auth, updateAddressController)
-addressRouter.delete("/delete", auth, updateAddressController)
+addressRouter.delete("/delete", auth, deleteAddressController)
 
 export default addressRouter
