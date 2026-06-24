4447994997c87d42bee97005a0ab3fdcb9b92a1f
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-06-24 19:25:11 +0530

    wip

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -134,8 +134,7 @@ const getOrderProductItem = async ({ lineItems, userId, addressId, paymentId, pa
         for (const item of lineItems.data) {
 
             const product = await Stripe.products.retrieve(item.price.product)
-            console.log("product", product)
-            console.log("item", item)
+            
 
             const payload = {
                 userId: userId,
